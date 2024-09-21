323a840c44715b3e7119bf4531e7dff49564b645
nath-d <nath.deborshi0711@gmail.com>
2024-09-21 19:05:56 +0530

    slight changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,21 +5,21 @@ const Hero = () => {
     return (
         <>
             <div className="relative h-screen w-full overflow-hidden">
-                <div className="absolute top-0 left-0 h-screen w-full bg-[url('/signage-pictures/open.jpg')] bg-cover bg-center z-0"></div>
+                <div className="absolute top-0 left-0 h-screen w-full bg-[url('/signage-pictures/open.jpg')] bg-cover bg-center"></div>
                 <div className="absolute top-0 left-0 h-screen w-full bg-black/70 backdrop-blur-sm"></div>
                 <div className="relative z-10">
                     <Navbar />
                     <div className='flex justify-start items-center h-screen p-4'>
-                        <div className='xl:ml-48 mt-[-220px] w-11/12 md:w-3/5 lg:w-1/2 xl:w-2/5 h-fit md:p-20 p-12 flex flex-col shadow-lg 
-                rounded-3xl bg-gray-950/10 ring-1 ring-black/5 backdrop-blur-md border-8 border-white'>
+                        <div className='xl:ml-48 mt-[-140px] w-11/12 md:w-3/5 lg:w-1/2 xl:w-2/5 h-fit md:p-20 p-12 flex flex-col shadow-lg 
+                rounded-3xl bg-gray-950/10 ring-1 ring-black/5 backdrop-blur-md border-[#EAE4D6] border-2'>
                             <div className='text-start text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-white font-bold leading-tight'>
-                                <span>SIGNS THAT</span><br />
+                                <span className='text-[#EAE4D6]'>SIGNS THAT</span><br />
                                 <span className='text-red-600'>SHINE</span>
                             </div>
-                            <div className='text-start text-lg md:text-xl lg:text-2xl xl:text-3xl text-white font-extralight mt-4'>
+                            <div className='text-start text-lg md:text-xl lg:text-2xl xl:text-3xl text-[#EAE4D6] font-extralight mt-4'>
                                 From concept to creation, we bring your narrative to life.
                             </div>
-                            <button className='bg-white text-start p-4 text-xl mt-8 w-fit rounded-xl text-red-600 font-medium'>EXPLORE NOW</button>
+                            <button className='bg-[#EAE4D6] text-start p-4 text-md md:text-xl mt-8 w-1/2 md:w-fit rounded-xl text-red-600 font-medium'>EXPLORE NOW</button>
                         </div>
                     </div>
                 </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,26 +1,44 @@
-import React from 'react'
+'use client'
+
+import { React, useState } from 'react'
 import { LuMenuSquare } from "react-icons/lu";
 
 const Navbar = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
+
     return (
-        <div className='w-full h-[180px] bg-transparent backdrop-blur-md items-center flex mx-auto justify-center text-white'>
-            <div className='flex justify-between items-center 2xl:w-[80%] w-full lg:p-12 p-2'>
+        <div className='w-full h-[140px] bg-transparent backdrop-blur-md items-center flex mx-auto justify-center text-white'>
+            <div className='flex justify-between items-center 2xl:w-[80%] w-full lg:p-2 p-2'>
                 {/* <div className='font-semibold text-4xl xl:text-6xl'>Blaze Signage</div> */}
-                <img src='logo/blaze-light.png' className='xl:w-fit w-[300px]'></img>
+                <img src='logo/newBlaze-light.png' className='xl:w-[400px] w-[300px]'></img>
                 {/* <div className='text-6xl font-semibold'>
                     Blaze Signage
                 </div> */}
-                <div className='lg:flex gap-6 lg:text-xl 2xl:text-2xl items-center hidden'>
+                <div className='lg:flex gap-6 lg:text-xl 2xl:text-2xl items-center hidden text-[#EAE4D6]'>
                     <div>Home</div>
                     <div>Services</div>
                     <div>Products</div>
                     <div>Clients</div>
                     <div>Gallery</div>
-                    <div className='ml-4 bg-white px-4 py-2 rounded-md text-black'>Contact Us</div>
+                    <div className='ml-4 bg-[#EAE4D6] px-4 py-2 rounded-md text-black'>Contact Us</div>
                 </div>
                 <div className='lg:hidden'>
-                    <LuMenuSquare className='lg:w-[40px] lg:h-[40px] w-[30px] h-[30px]' />
+                    <LuMenuSquare onClick={toggleMenu} className='lg:w-[40px] lg:h-[40px] w-[30px] h-[30px] text-[#EAE4D6]' />
                 </div>
+                {isMenuOpen && (
+                    <div className='lg:hidden flex flex-col items-center text-[#EAE4D6] bg-blue-200 w-full h-screen absolute top-[140px] z-50'>
+                        <div className='py-2'>Home</div>
+                        <div className='py-2'>Services</div>
+                        <div className='py-2'>Products</div>
+                        <div className='py-2'>Clients</div>
+                        <div className='py-2'>Gallery</div>
+                        <div className='py-2 bg-[#EAE4D6] text-black px-4 rounded-md'>Contact Us</div>
+                    </div>
+                )}
 
             </div>
         </div>
